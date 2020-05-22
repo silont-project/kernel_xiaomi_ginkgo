c655a528513f6ef96bd9cdec03989eadc57f1448
celtare21 <celtare21@gmail.com>
2020-05-22 11:55:51 +0200

    drivers: usb: Fast-forward to latest CAF tag
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Change-Id: I9f8eca155d8364f4dd6026c2e923612e02e8054a

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1282,6 +1282,9 @@ static void dwc3_get_properties(struct dwc3 *dwc)
 	dwc->dis_metastability_quirk = device_property_read_bool(dev,
 				"snps,dis_metastability_quirk");
 
+	dwc->dis_metastability_quirk = device_property_read_bool(dev,
+				"snps,dis_metastability_quirk");
+
 	dwc->lpm_nyet_threshold = lpm_nyet_threshold;
 	dwc->tx_de_emphasis = tx_de_emphasis;
 
